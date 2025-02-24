c8f664160110c5436292e808d923e7eda89197ea
ifeoluwa <onabajoif@yahoo.com>
2025-02-24 15:20:03 +0100

    using a remote db

--- util/database.js
+++ util/database.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('swinemp', 'aiquad', 'aiquad@965', { host: 'localhost', dialect: 'postgres' });
+// const sequelize = new Sequelize('swinemp', 'aiquad', 'aiquad@965', { host: 'localhost', dialect: 'postgres' });
+const sequelize = new Sequelize('postgresql://swinemp_user:4BLg4sW0Qd1q1Ef0mCcC9ZQnEhXV1dAu@dpg-cuu7jfqj1k6c738jtobg-a.singapore-postgres.render.com/swinemp?ssl=true');
 
 module.exports = sequelize;
